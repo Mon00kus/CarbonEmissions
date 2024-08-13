053d7bbffc95aab16e528eb31edca59a5f513d06
mon00kus <raulmoncadac@gmail.com>
2024-08-13 14:55:10 +0200

    Implementacion de test de controller y de uniofwork emissions

---
+++ CarbonEmissions/CarbonEmissions.Test/Controllers/EmissionsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using CarbonEmissions.Backend.Controllers;
+using CarbonEmissions.Backend.UnitOfWork.Interfaces;
+using CarbonEmissions.Shared.Dtos;
+using CarbonEmissions.Shared.Entities;
+using CarbonEmissions.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CarbonEmissions.Test.Controllers
+{
+    [TestClass]
+    public class EmissionsControllerTests
+    {
+        private Mock<IGenericUnitOfWork<Emission>> _mockGenericUnitOfWork = null!;
+        private Mock<IEmissionsUnitOfWork> _mockEmissionsUnitOfWork = null!;
+        private EmissionsController _controller = null!;
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Emission>>();
+            _mockEmissionsUnitOfWork = new Mock<IEmissionsUnitOfWork>();
+            _controller = new EmissionsController(_mockGenericUnitOfWork.Object, _mockEmissionsUnitOfWork.Object);
+        }
+        [TestMethod]
+        public async Task GetAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new ActionResponse<IEnumerable<Emission>> { WasSuccess = true };
+            _mockEmissionsUnitOfWork.Setup(x => x.GetAsync()).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(response.Result, okResult!.Value);
+            _mockEmissionsUnitOfWork.Verify(x => x.GetAsync(), Times.Once());
+        }
+    }
+}
---
+++ CarbonEmissions/CarbonEmissions.Test/UnitOfWprk/EmissionsUnitOfWorkTest.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using CarbonEmissions.Backend.Repositories;
+using CarbonEmissions.Backend.UnitOfWork;
+using CarbonEmissions.Shared.Dtos;
+using CarbonEmissions.Shared.Entities;
+using CarbonEmissions.Shared.Responses;
+using CarbonEmissions.Backend.Repositories.Implementations;
+using CarbonEmissions.Backend.Repositories.Interfaces;
+using CarbonEmissions.Backend.UnitOfWork.Implementations;
+
+
+namespace CarbonEmissions.Test.UnitOfWprk
+{
+    [TestClass]
+    public class EmissionsUnitOfWorkTest
+    {
+        private Mock<IGenericRepository<Emission>> _repositoryUnitMock = null!;
+        private Mock<IEmissionsRepository> _mockEmissionsRepository = null!;
+        private EmissionsUnitOfWork _unitOfWork = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repositoryUnitMock = new Mock<IGenericRepository<Emission>>();
+            _mockEmissionsRepository = new Mock<IEmissionsRepository>();
+            _unitOfWork = new EmissionsUnitOfWork(_repositoryUnitMock.Object, _mockEmissionsRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task Get_Async_CallsRepositoryAndReturnResult()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var expectedActionResponse = new ActionResponse<IEnumerable<Emission>> { Result = new List<Emission>() };
+            _mockEmissionsRepository.Setup(x => x.GetAsync()).ReturnsAsync(expectedActionResponse);
+
+            // Act
+            var result = await _unitOfWork.GetAsync();
+
+            // Assert
+            Assert.AreEqual(expectedActionResponse, result);
+            _mockEmissionsRepository.Verify(x => x.GetAsync(), Times.Once);
+
+        }
+    }
+}
